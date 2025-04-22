37822e073637bee27611122518441981f81c5ee6
khambaleevvv <khambaleevvv@multicarta.ru>
2025-04-22 16:56:34 +0300

    Добавил решение по задаче №5

--- module_04_flask/homework/hw5/ps.py
+++ module_04_flask/homework/hw5/ps.py
@@ -7,15 +7,25 @@
 
 /ps?arg=a&arg=u&arg=x
 """
+import shlex
+import subprocess
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/ps", methods=["GET"])
 def ps() -> str:
-    ...
+    get_args: list[str] = request.args.getlist('arg')
+    if get_args:
+        args = shlex.quote(''.join(elem for elem in get_args))
+        result = subprocess.check_output(f"ps {args}", shell=True).decode()
+    else:
+        result = subprocess.check_output(f"ps", shell=True).decode()
+
+    return f"<pre>{result}</pre>"
+
 
 
 if __name__ == "__main__":
