e349fd5f3b7cf6a53a5e1edea05da2793fc51703
khambaleevvv <khambaleevvv@multicarta.ru>
2025-05-02 09:40:12 +0300

    Добавил решение по задаче №2

--- module_06_debugging_begin/homework/hw1_2/authentication.py
+++ module_06_debugging_begin/homework/hw1_2/authentication.py
@@ -18,11 +18,33 @@
 import getpass
 import hashlib
 import logging
+from typing import List
+import re
 
 logger = logging.getLogger("password_checker")
 
+words_file = "C:/Users/khambaleevvv/Downloads/words.txt"
 
-def is_strong_password(password: str) -> bool:
+def get_list_of_words_from_file(path:str) -> set:
+    set_of_words = set()
+    with open(path, 'r', encoding='utf8') as file:
+        for i_line in file.readlines():
+            if i_line.endswith('\n'):
+                clear_line = i_line[:-2]
+                if len(clear_line) > 4:
+                    set_of_words.add(clear_line)
+    return set_of_words
+
+
+def is_strong_password(password: str, set_of_words: set) -> bool:
+    password_lower_register = password.lower()
+    pattern = r'[a-zA-Z]+'
+    words_in_password = re.findall(pattern, password_lower_register)
+    for i_word in words_in_password:
+        if i_word in set_of_words:
+            logger.info(f"Слово {i_word} БЫЛО найдено в словаре.")
+            return False
+    logger.info(f"Слова {words_in_password} не найдено|ы в словаре.")
     return True
 
 
@@ -33,9 +55,10 @@ def input_and_check_password() -> bool:
     if not password:
         logger.warning("Вы ввели пустой пароль.")
         return False
-    elif is_strong_password(password):
-        logger.warning("Вы ввели слишком слабый пароль")
+    elif not is_strong_password(password, set_of_words=words_set):
+        logger.warning("Вы ввели слишком слабый пароль!")
         return False
+    logger.warning("Пароль соответствует требованиям безопасности!")
 
     try:
         hasher = hashlib.md5()
@@ -51,6 +74,7 @@ def input_and_check_password() -> bool:
 
 
 if __name__ == "__main__":
+    words_set = get_list_of_words_from_file(words_file)
     FORMAT = '%(asctime)s %(name)s %(levelname)s %(message)s'
     custom_time_format = "%H:%M:%S"
     logging.basicConfig(level=logging.INFO, filename="stderr.txt",
