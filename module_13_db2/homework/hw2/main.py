d394303bf95c25384045a01d79152dec018c648c
Vadim Khambaleev VM <khambaleevvv@multicarta.ru>
2025-05-30 18:03:32 +0500

    Добавил решение по задаче №2.

--- module_13_db2/homework/hw2/main.py
+++ module_13_db2/homework/hw2/main.py
@@ -1,11 +1,21 @@
 import sqlite3
+import csv
 
+sql_request_to_delete = """
+DELETE FROM table_fees 
+WHERE truck_number = ? 
+AND timestamp = ?
+"""
 
 def delete_wrong_fees(
         cursor: sqlite3.Cursor,
         wrong_fees_file: str
 ) -> None:
-    ...
+    with open(wrong_fees_file, newline='') as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=',')
+        for row in reader:
+            cursor.execute(sql_request_to_delete, (row['car_number'], row['timestamp']))
+            # print(row['car_number'], row['timestamp'])
 
 
 if __name__ == "__main__":
