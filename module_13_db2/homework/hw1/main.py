7d1a5990d9377639b3fb7eba1eb2f4a5f4efde41
Vadim Khambaleev VM <khambaleevvv@multicarta.ru>
2025-05-30 12:35:45 +0500

    Добавил решение по задаче №1.

--- module_13_db2/homework/hw1/main.py
+++ module_13_db2/homework/hw1/main.py
@@ -1,12 +1,28 @@
 import sqlite3
 
 
+sql_request = """
+SELECT EXISTS(
+SELECT 1, 
+SUM(CASE WHEN temperature_in_celsius < 16 OR temperature_in_celsius > 20 THEN 1 ELSE 0 END)
+AS violations
+FROM table_truck_with_vaccine
+WHERE truck_number = ?
+GROUP BY truck_number
+HAVING
+SUM(CASE WHEN temperature_in_celsius < 16 OR temperature_in_celsius > 20 THEN 1 ELSE 0 END) > 3
+);
+"""
+
+
 def check_if_vaccine_has_spoiled(
         cursor: sqlite3.Cursor,
         truck_number: str
 ) -> bool:
-    ...
-
+    cursor.execute(sql_request, (truck_number, ))
+    result = cursor.fetchone()
+    # print(result)
+    return result[0]
 
 if __name__ == '__main__':
     truck_number: str = input('Введите номер грузовика: ')
